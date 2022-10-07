e53651d6d4fc97676c3f85d20257c68eddde828d
DinhNVT <huudinhnguyen001k@gmail.com>
2022-10-07 17:45:13 +0700

    First commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const App = () => {
+  return (
+    <View>
+      <Text>teach e</Text>
+      <Icon name="rocket" size={30} color="#900" />
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
